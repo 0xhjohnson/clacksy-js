f72352b64ab8138bb297c508dadb2a02ba7125d5
Hunter Johnson <0xhjohnson@protonmail.com>
2022-07-02 21:08:38 -0400

    feat: added proper meta tag for 2022

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import type { AppProps } from 'next/app'
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export { reportWebVitals } from 'next-axiom'
 
@@ -12,14 +13,19 @@ function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <UserProvider supabaseClient={supabaseClient}>
-          <Component {...pageProps} />
-          <ReactQueryDevtools />
-        </UserProvider>
-      </Hydrate>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <UserProvider supabaseClient={supabaseClient}>
+            <Component {...pageProps} />
+            <ReactQueryDevtools />
+          </UserProvider>
+        </Hydrate>
+      </QueryClientProvider>
+    </>
   )
 }
 
