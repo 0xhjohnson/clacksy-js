c47ca7c947d65fcba4d1a3a2045d07200a72ad66
Hunter Johnson <0xhjohnson@protonmail.com>
2022-07-28 20:43:20 -0400

    feat: turn on experimental browsersList for modern browsers

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ const { withAxiom } = require('next-axiom')
 /** @type {import('next').NextConfig} */
 module.exports = withAxiom({
   reactStrictMode: true,
+  experimental: {
+    browsersListForSwc: true,
+    legacyBrowsers: false
+  }
 })
